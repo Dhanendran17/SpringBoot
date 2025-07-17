a6900c4e62b04bbc11531770879e3f9797ab1163
Dhanendran17 <dhanendransiva@gmail.com>
2025-07-17 18:06:46 +0530

    Initial Commit

---
+++ src/main/java/com/example/FirstSpringBoot/Student.java
@@ -0,0 +1,17 @@
+package com.example.FirstSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student
+{
+    int age;
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void show()
+    {
+        System.out.println("Hey World");
+    }
+}
